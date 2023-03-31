5fb9ab51073440802b5f9819cc776d6f53e5a421
RafaEscobar <rafael.eg.code@gmail.com>
2023-03-30 23:23:48 -0600

    Test de funcionalidad

--- resources/views/faltas.blade.php
+++ resources/views/faltas.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header card-header-primary">
-                            <h4 class="card-title ">Inasistencias: {{$cantidad}}</h4>
+                            <h4 class="card-title ">Inasistencias: #{{$cantidad}}</h4>
                         </div>
 
                         <div class="card-body">
